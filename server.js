6ce3d67ccf53e2e7c9b39e2b43eb7a6764e1a082
Kitti <kittitoth8@gmail.com>
2023-04-15 23:24:27 +0200

    set up mongodb connection

--- server.js
+++ server.js
@@ -1,7 +1,34 @@
 const express = require('express');
 const app = express();
+const { MongoClient } = require('mongodb');
+const dotenv = require('dotenv').config({path: __dirname + '/.env'});
 const port = process.env.PORT || 3000;
 const path = require('path');
+
+
+
+async function main(){
+  const client = new MongoClient(dotenv.parsed.CONNECTION_STRING, {
+    useNewUrlParser: true,
+    dbName: 'balance-sheet',
+  });
+
+  try {
+      // Connect to the MongoDB cluster
+      await client.connect();
+
+      // Make the appropriate DB calls
+      await listDatabases(client);
+
+  } catch (e) {
+      console.error(e);
+  } finally {
+      await client.close();
+  }
+}
+
+main().catch(console.error);
+
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/index.html'));
