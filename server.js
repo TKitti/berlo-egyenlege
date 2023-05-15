ee4765f2808290cf804f5aa9f7dc3187960bef75
Kitti <kittitoth8@gmail.com>
2023-05-15 21:38:49 +0200

    send response on / domain

--- server.js
+++ server.js
@@ -24,6 +24,12 @@ app.use(cors());
 app.use(express.json());
 app.use(router);
 
+
+app.get("/", (req, res) => {
+  res.send("test");
+});
+
+
 app.listen(port, ()=> {
   console.log("listening to the server on http://localhost:3000")
 });
