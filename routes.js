7e424d4721f7f6c58970fe5d1fd7a779e2cc8340
Kitti <kittitoth8@gmail.com>
2023-05-19 14:20:37 +0200

    remove body parser package

--- routes.js
+++ routes.js
@@ -1,17 +1,6 @@
 const express = require('express');
-//const app = express();
-//const path = require('path');
-//const bodyparser = require('body-parser')
 const { getCostService, createCostService } = require('./database/services/cost.service');
 const { getBalanceService } = require('./database/services/balance.service');
-
-
-// Body-parser middleware
-/*
-app.use(bodyparser.urlencoded({ extended: true }))
-app.use(bodyparser.json())
-*/
-
 const router = express.Router();
 
 router.get("/", (req,res)=>res.send("teszt"));
